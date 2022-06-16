8fcc52ef93ab9789ab5cb854ca1aae60a4f6f439
Marat <zeuzs@yandex.ru>
2022-06-16 09:48:22 -0700

    dobacili i nastroili load balancer

--- main.tf
+++ main.tf
@@ -8,10 +8,17 @@ variable "server_port" {
     default = 8080  
 }
 
+#eto dlya odnogo servera
 #output "public_ip" {
 #    value = aws_instance.example.public_ip
 #    description = "The public IP address of the my web server"  
 #}
+
+#eto dlya balancera
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
+}
 
 data "aws_vpc" "default" {
     default = true
@@ -43,18 +50,104 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
-    launch_configuration = aws_launch_configuration.example.name
-    vpc_zone_identifier = data.aws_subnets.default.ids
-
-    min_size = 2
-    max_size = 10
-
-    tag {
-      key = "Name"
-      value = "terraform-asg-example"
-      propagate_at_launch = true
-    }  
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  
+  min_size = 2
+  max_size = 10
+  
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }  
 }
+
+resource "aws_lb" "example" {
+  name = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnets.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    #po umol4aniyu vernet pustuyu stroku s kodom 404
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "404: page not found"
+        status_code = 404
+      }
+    }
+  
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  #razresh-m vse in http-zaprosi
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  #razr-m vse out zaprosi
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-asg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+  
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  condition {
+    #old version...
+    #field = "path-pattern "
+    #values = ["*"]
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
 
 resource "aws_security_group" "instance" {
     name = "terraform-example-instance"
